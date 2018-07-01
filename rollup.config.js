134fa0c019322f3dca103076ead695c9c30b9de1
theboolean <cesana.lorenzo@gmail.com>
2018-07-01 15:53:08 +0200

    build: rollup scripts

---
+++ rollup.config.js
@@ -0,0 +1,33 @@
+import babel from 'rollup-plugin-babel'
+import commonjs from 'rollup-plugin-commonjs'
+
+/* eslint import/no-default-export: 0 */
+
+export default {
+  input: 'src/index.js',
+  output: [
+    {
+      file: 'dist/bundle.cjs.js',
+      format: 'cjs'
+    },
+    {
+      file: 'dist/bundle.esm.js',
+      format: 'es'
+    }
+  ],
+  external: [
+    'country-data',
+    'moment-timezone',
+    'moment-timezone/data/meta/latest.json',
+    'ua-parser-js'
+  ],
+  plugins: [
+    commonjs({
+      include: 'node_modules/**'
+    }),
+    babel({
+      exclude: 'node_modules/**',
+      plugins: ['external-helpers']
+    })
+  ]
+}
